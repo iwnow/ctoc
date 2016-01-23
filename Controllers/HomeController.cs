20ff419bc49401391839820d7e2ec9e6de0bcbd3
iwnow <vostroknutov@hotmail.com>
2016-01-23 20:10:58 +0300

    -c

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,9 +16,11 @@ namespace Call2Collegs{
         public HomeController(IDataAccessProvider dataProvider){
             this._dataProvider = dataProvider;
         }
+        
+        static long _id = 0;
         public IActionResult Index(){
             //Console.WriteLine(ViewBag.val);
-            var u = new User { Id = 0, Login = "tester", Password = "pass"};
+            var u = new User { Id = ++_id, Login = "tester", Password = "pass"};
             _dataProvider.Add(u);
             var all = _dataProvider.Get();
             return View(all);
--- readme.md
+++ readme.md
@@ -5,5 +5,5 @@
 
 ### Docker
 #### DNX patch for linux Socket.Select() method
-`COPY ./System.Native.so /opt/DNX_BRANCH/runtimes/dnx-coreclr-linux-x64.1.0.0-rc1-update1/bin`
+`COPY ./dnxpatch/System.Native.so /opt/DNX_BRANCH/runtimes/dnx-coreclr-linux-x64.1.0.0-rc1-update1/bin`
 [issue](https://github.com/dotnet/corefx/issues/4631#issuecomment-165893079)
